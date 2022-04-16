2a7e26f53595cddb86b80d976297d24b16c3c74b
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-04-16 14:33:27 -0300

    [transaction-api] connect to redis when starting the server

--- transaction-api/src/server.ts
+++ transaction-api/src/server.ts
@@ -7,6 +7,7 @@ import entryPoint from '@/presentation/entry-point';
 import entryPointValidationMiddleware from '@/presentation/middleware/entry-point-validation-middleware';
 
 import mongoHelper from './infra/mongo-helper';
+import redisHelper from './infra/redis-helper';
 
 const app = express();
 
@@ -18,5 +19,11 @@ mongoHelper.connect(env.mongoUri)
   .then(() => {
     console.log('MongoDB connected');
 
-    app.listen(env.port, () => console.log('transaction-api is running..'));
+    redisHelper.redisClient
+      .connect()
+      .then(() => {
+        console.log('Redis connected');
+
+        app.listen(env.port, () => console.log('transaction-api is running..\n\n'));
+      });
   });
