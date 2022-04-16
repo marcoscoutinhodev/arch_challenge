e21244a103a616b72436027969894f576c4d4cab
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-04-16 13:48:58 -0300

    [transaction-api] set server entry point route

--- transaction-api/src/server.ts
+++ transaction-api/src/server.ts
@@ -1,8 +1,22 @@
+/* eslint-disable no-console */
+import 'module-alias/register';
 import express from 'express';
+import env from '@/config/env';
+
+import entryPoint from '@/presentation/entry-point';
+import entryPointValidationMiddleware from '@/presentation/middleware/entry-point-validation-middleware';
+
+import mongoHelper from './infra/mongo-helper';
 
 const app = express();
 
 app.use(express.json());
 
-// eslint-disable-next-line no-console
-app.listen(4001, () => console.log('transaction-api is running..'));
+app.post('/', entryPointValidationMiddleware, entryPoint.handle);
+
+mongoHelper.connect(env.mongoUri)
+  .then(() => {
+    console.log('MongoDB connected');
+
+    app.listen(env.port, () => console.log('transaction-api is running..'));
+  });
