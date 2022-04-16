3c7768bdebe2a73f3e7d7ba0c600e38f26f2c26f
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-04-16 14:41:14 -0300

    [api-gateway] fix proxy usage

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/transaction', httpProxy('http://127.0.0.1:4001', { timeout: 3000 }));
-app.get('/balance/:id', httpProxy('http://127.0.0.1:4002', { timeout: 3000 }));
+app.use('/transaction', httpProxy('http://127.0.0.1:4001/', { timeout: 3000 }));
+app.use('/balance/:id', httpProxy('http://127.0.0.1:4002', { timeout: 3000 }));
 
 // eslint-disable-next-line no-console
 app.listen(4000, () => console.log('API Gateway running...'));
